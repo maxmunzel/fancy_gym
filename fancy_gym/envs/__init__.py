8743fff36930d629dda96447fd765bcf053f9488
ii9715@partner.kit.edu <ii9715@uc2n994.localdomain>
2024-03-15 14:37:22 +0100

    Sweep42 - 38 but back to 4 basis

--- fancy_gym/envs/__init__.py
+++ fancy_gym/envs/__init__.py
@@ -269,6 +269,42 @@ DEFAULT_BB_DICT_ProDMP = {
 }
 # Max
 register(
+        id=f"Sweep42",
+        entry_point='fancy_gym.utils.make_env_helpers:make_bb_env_helper',
+        kwargs={
+            "name": "BoxPushingTemporalSparse-v0",
+            "wrappers": [mujoco.box_pushing.MPWrapper],
+            "trajectory_generator_kwargs": {
+                "trajectory_generator_type": "prodmp",
+                "duration": 4.0,
+                "action_dim": 2,
+                "weight_scale": 0.3,
+                "auto_scale_basis": True,
+                "goal_scale": 0.5,
+                "relative_goal": False,
+                "disable_goal": False,
+            },
+            "phase_generator_kwargs": {
+                'phase_generator_type': 'exp',
+                'tau': 2.4,
+            },
+            "controller_kwargs": {
+                "controller_type": "position",
+                },
+            "basis_generator_kwargs": {
+                'basis_generator_type': 'prodmp',
+                'alpha': 10,
+                "num_basis": 4,
+                # 'num_basis_zero_start': 1,
+            },
+            #'black_box_kwargs': {
+            #    'replanning_schedule': lambda pos, vel, obs, action, t : t % 50 == 0,
+            #    'condition_on_desired': True,
+            #},
+                "random_init": True,
+            }
+    )
+register(
         id=f"Sweep38",
         entry_point='fancy_gym.utils.make_env_helpers:make_bb_env_helper',
         kwargs={
