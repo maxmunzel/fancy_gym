78b4148a8f4f81206b014221fc53a9eba7073231
ii9715@partner.kit.edu <ii9715@uc2n996.localdomain>
2024-04-09 09:41:39 +0200

    Simplify reward

--- fancy_gym/envs/mujoco/box_pushing/box_pushing_env.py
+++ fancy_gym/envs/mujoco/box_pushing/box_pushing_env.py
@@ -623,27 +623,10 @@ class BoxPushingDense(BoxPushingEnvBase):
         qvel,
         action,
     ):
-        joint_penalty = self._joint_limit_violate_penalty(
-            qpos, qvel, enable_pos_limit=True, enable_vel_limit=True
-        )
-        tcp_box_dist_reward = -2 * np.clip(
-            np.linalg.norm(box_pos - rod_tip_pos), 0.05, 100
-        )
         box_goal_pos_dist_reward = -3.5 * np.linalg.norm(box_pos - target_pos)
         box_goal_rot_dist_reward = -rotation_distance(box_quat, target_quat) / np.pi
-        energy_cost = 0  # -0.0005 * np.sum(np.square(action))
 
-        reward = (
-            joint_penalty
-            + tcp_box_dist_reward
-            + box_goal_pos_dist_reward
-            + box_goal_rot_dist_reward
-            + energy_cost
-        )
-
-        rod_inclined_angle = rotation_distance(rod_quat, self._desired_rod_quat)
-        if rod_inclined_angle > np.pi / 4:
-            reward -= rod_inclined_angle / (np.pi)
+        reward = box_goal_pos_dist_reward + box_goal_rot_dist_reward
 
         return reward
 
@@ -668,22 +651,6 @@ class BoxPushingTemporalSparse(BoxPushingEnvBase):
         action,
     ):
         reward = 0.0
-        joint_penalty = self._joint_limit_violate_penalty(
-            qpos, qvel, enable_pos_limit=True, enable_vel_limit=True
-        )
-        energy_cost = 0  # -0.02 * np.sum(np.square(action))
-        tcp_box_dist_reward = -2 * np.clip(
-            np.linalg.norm(box_pos - rod_tip_pos), 0.05, 100
-        )
-        reward += joint_penalty + tcp_box_dist_reward + energy_cost
-        rod_inclined_angle = rotation_distance(rod_quat, desired_rod_quat)
-
-        if rod_inclined_angle > np.pi / 4:
-            reward -= rod_inclined_angle / (np.pi)
-
-        if not episode_end:
-            return reward
-
         box_goal_dist = np.linalg.norm(box_pos - target_pos)
 
         box_goal_pos_dist_reward = -3.5 * box_goal_dist * 100
@@ -691,9 +658,7 @@ class BoxPushingTemporalSparse(BoxPushingEnvBase):
             -rotation_distance(box_quat, target_quat) / np.pi * 100
         )
 
-        ep_end_joint_vel = -50.0 * np.linalg.norm(qvel)
-
-        reward += box_goal_pos_dist_reward + box_goal_rot_dist_reward + ep_end_joint_vel
+        reward += box_goal_pos_dist_reward + box_goal_rot_dist_reward
 
         return reward
 
