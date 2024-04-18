609fca453200505889730e4bfc28eb420b8ca8c2
Max Nagy <maxmunzel@me.com>
2024-04-18 10:29:41 +0200

    Simplify playstation.py

--- fancy_gym/examples/playstation.py
+++ fancy_gym/examples/playstation.py
@@ -1,16 +1,13 @@
 import pygame
 import numpy as np
 import time
-from fancy_gym.envs.mujoco.box_pushing.box_pushing_env import BoxPushingDense
+from fancy_gym.envs.mujoco.box_pushing.box_pushing_env import (
+    BoxPushingDense,
+    BoxPushingTemporalSparse,
+)
 from fancy_gym.envs.mujoco.box_pushing.throttle import Throttle
 import fancy_gym
 import os
-
-FPS = 40
-dt = BoxPushingDense().dt
-skip = int(0.9 * (1 / FPS) / dt)
-skip = max(1, skip)
-print(skip)
 
 # Initialize pygame and the joystick
 pygame.init()
@@ -19,66 +16,32 @@ joystick = pygame.joystick.Joystick(0)  # Assume the PS4 controller is joystick 
 joystick.init()
 
 # Setup OpenAI Gym environment
-env = BoxPushingDense(random_init=True, frame_skip=skip)
+env = BoxPushingTemporalSparse(random_init=True, frame_skip=10)
 # env = fancy_gym.make("BoxPushingDense-v0", seed=42)
 obs = env.reset(seed=42)
 
-cum_reward = 0
-start_time = time.time()
-frame = 0
 throttle = Throttle(target_hz=50, busy_wait=False)
-while True:
-    frame += 1
-    throttle.tick()
-    for event in pygame.event.get():
-        # If you want to handle specific events, you can do that here
-        if event.type == pygame.JOYBUTTONDOWN:
-            if joystick.get_button(0):  # X button is typically indexed at 0
-                obs = env.reset()
-                cum_reward = 0
-        pass
+try:
+    while True:
+        throttle.tick()
+        for event in pygame.event.get():
+            # If you want to handle specific events, you can do that here
+            if event.type == pygame.JOYBUTTONDOWN:
+                if joystick.get_button(0):  # X button is typically indexed at 0
+                    obs = env.reset()
+                    cum_reward = 0
+            pass
 
-    # Read joystick axes for actions
-    x_axis = joystick.get_axis(0)  # Left stick horizontal
-    y_axis = joystick.get_axis(1)  # Left stick vertical
+        x_axis = joystick.get_axis(0)  # Left stick horizontal
+        y_axis = joystick.get_axis(1)  # Left stick vertical
 
-    # Here, I assume the environment expects a 2D action.
-    # Adjust this as per the action space of your environment.
+        joy = np.array([y_axis, x_axis])
 
-    finger_pos = env.data.body("finger").xpos.copy()
-    action = finger_pos[:2] + 2* np.array([y_axis, x_axis])
-    action *= 1
+        finger_pos = env.data.body("finger").xpos.copy()[:2]
+        action = finger_pos + 2 * joy
+        action *= 1
+        obs, reward, done, info = env.step(joy)
 
-    # print(action)
-
-    # Step through the environment with the chosen action
-    obs, reward, done, info = env.step(action)
-    cum_reward *= 0.99
-    cum_reward += reward
-
-    # Render the environment
-    if "REDIS_IP" in os.environ:
-        if not frame % 20:
-            env.render(mode="human")
-    else:
-        if not frame % 4:
-            env.render(mode="human")
-
-    # print(f"Reward: {cum_reward:3.2f} vel: {env.data.qpos[:7]}")
-
-    # keep simulation running in approximate real time
-    t_real = time.time() - start_time
-    t_sim = frame * dt
-    error = t_sim - t_real
-
-    if error > 0:
-        pass
-        # time.sleep(error)
-    if "REDIS_IP" in os.environ and abs(error) > 10:
-        # we don't want the simulation to catch up to the real time,
-        # because this could cause exessively fast movements
-        print("Error, simulation lagging behind!")
-        break
-
-
-pygame.quit()
+        env.render(mode="human")
+finally:
+    pygame.quit()
