d363d3c8c88955d4469b3d9d32952a328f73cd06
ii9715@partner.kit.edu <ii9715@uc2n995.localdomain>
2024-04-23 13:26:23 +0200

    Sweep68 - BB -- 67 but harsher rotation, no speed penality, varying weight_scales

--- fancy_gym/envs/mujoco/box_pushing/box_pushing_env.py
+++ fancy_gym/envs/mujoco/box_pushing/box_pushing_env.py
@@ -257,7 +257,7 @@ class BoxPushingEnvBase(MujocoEnv, utils.EzPickle):
         else:
             reward = -50
 
-        if episode_end:
+        if episode_end and False:
             # Max EE Speed Panality -- ensure the trajectory is executable
             # Polymetis seems to only have joint speed limits but the following limit is based on the max ee speed
             # during the rollouts of Sweep47.
