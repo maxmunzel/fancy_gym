5ee5013ac711eb1b751d12f7a36b47f412c2574e
ii9715@partner.kit.edu <ii9715@uc2n995.localdomain>
2024-07-20 16:03:20 +0200

    Increade Target success to 90%

--- fancy_gym/envs/mujoco/box_pushing/box_pushing_env.py
+++ fancy_gym/envs/mujoco/box_pushing/box_pushing_env.py
@@ -158,7 +158,7 @@ class BoxPushingEnvBase(MujocoEnv, utils.EzPickle):
             dist=dist,
             k=200,
             kl_bound=0.2,
-            target_success_rate=0.6,
+            target_success_rate=0.9,
         )
         # make it a little simpler for me to write the ymls
         # print("\n".join(f'"{k}",' for k in self.doraemon.param_dict().keys()))
